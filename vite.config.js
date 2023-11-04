dedccaf4f443a85682ad1bcd79e1c144164c34e9
tyzrex <sulavbaral58@gmail.com>
2023-11-04 10:25:18 +0545

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
+  base: "/ThreeJs-Coming-Soon",
 });
